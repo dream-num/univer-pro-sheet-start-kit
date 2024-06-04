b517e6dea93fe025d96c283726e53cf232be3385
hexf00 <hexf00@gmail.com>
2024-06-04 12:05:31 +0800

    chore: update to v0.1.13

--- src/locale.ts
+++ src/locale.ts
@@ -1,13 +1,13 @@
 import { LocaleType } from '@univerjs/core'
-import { enUS as UniverDesignEnUS } from '@univerjs/design'
-import { enUS as UniverDocsUIEnUS } from '@univerjs/docs-ui'
-import { enUS as UniverSheetsEnUS } from '@univerjs/sheets'
-import { enUS as UniverSheetsUIEnUS } from '@univerjs/sheets-ui'
-import { enUS as UniverUiEnUS } from '@univerjs/ui'
+import UniverDesignEnUS from '@univerjs/design/locale/en-US'
+import UniverDocsUIEnUS from '@univerjs/docs-ui/locale/en-US'
+import UniverSheetsEnUS from '@univerjs/sheets/locale/en-US'
+import UniverSheetsUIEnUS from '@univerjs/sheets-ui/locale/en-US'
+import UniverUiEnUS from '@univerjs/ui/locale/en-US'
 
-import { enUS as CollaborationClientEnUS } from '@univerjs-pro/collaboration-client'
-import { enUS as SheetsPrintEnUS } from '@univerjs-pro/sheets-print'
-import { enUS as UniverSheetsExchangeClientEnUS } from '@univerjs-pro/sheets-exchange-client'
+import CollaborationClientEnUS from '@univerjs-pro/collaboration-client/locale/en-US'
+import SheetsPrintEnUS from '@univerjs-pro/sheets-print/locale/en-US'
+import UniverSheetsExchangeClientEnUS from '@univerjs-pro/sheets-exchange-client/locale/en-US'
 
 export const locales = {
   [LocaleType.EN_US]: {
