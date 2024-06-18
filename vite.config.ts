1b591bf0bdd49327ac973b3d09e2e9fece31bee5
hexf00 <hexf00@gmail.com>
2024-06-18 16:42:18 +0800

    chore: fix host on wsl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '/universer-api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         ws: true,
       },
